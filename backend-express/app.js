ee7f2a816f93f4158afa09a14ec884bd812a09e0
predo <predoiu.tibi@gmail.com>
2023-04-22 21:07:01 +0300

    select calator endpoint

--- backend-express/app.js
+++ backend-express/app.js
@@ -53,6 +53,46 @@ app.get("/calator", (req, res) => {
 });
 
 
+async function selectAllGara(req, res) {
+    let connection;
+    let result;
+    try {
+      connection = await oracledb.getConnection({
+        user: "oltp",
+        password: "oltp",
+        connectString: "localhost:1521/datawproiect.docker.internal",
+      });
+      // run query to get employee with employee_id
+      result = await connection.execute(`SELECT * FROM GARA`);
+    } catch (err) {
+      //send error message
+      return res.send(err.message);
+    } finally {
+      if (connection) {
+        try {
+          // Always close connections
+          await connection.close();
+        } catch (err) {
+          return console.error(err.message);
+        }
+      }
+      console.log(result);
+      if (result.rows.length == 0) {
+        //query return zero employees
+        return res.send("query send no rows");
+      } else {
+        //send all employees
+        return res.send(result.rows);
+      }
+    }
+  }
+  
+
+  //GET /gara
+app.get("/gara", (req, res) => {
+    selectAllGara(req, res);
+  });
+
 
 const app = express();
 app.use(cors());
