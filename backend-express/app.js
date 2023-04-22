1390e5800d8b1fab7d28d3f53b76beac0a82d559
predo <predoiu.tibi@gmail.com>
2023-04-22 21:25:16 +0300

    server

--- backend-express/app.js
+++ backend-express/app.js
@@ -473,6 +473,20 @@ async function cerere5(req, res) {
   });
   
 
+  app.get("/", (req, res) => {
+    res.status(200);
+    res.send("Welcome to root URL of Server");
+  });
+  
+  app.listen(PORT, (error) => {
+    if (!error)
+      console.log(
+        "Server is Successfully Running, and App is listening on port " + PORT
+      );
+    else console.log("Error occurred, server can't start", error);
+  });
+  
+
 
 const app = express();
 app.use(cors());
